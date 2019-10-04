e2ffc5ba764ce7c4a98aadb7ba3650060272e96d
Thibault Coupin <thibault.coupin@gmail.com>
2019-10-04 14:47:44 +0200

    autocomplete doi

--- src/main/webapp/resources/js/horizon.js
+++ src/main/webapp/resources/js/horizon.js
@@ -11,11 +11,12 @@
      var inputs = $('div:has(>label[for="metadata_publicationIDNumber"])').find('input')
     
      if (inputs.toArray().indexOf(e.target)>=0){
-         var inputStr = $(e.target).val()
-         if (inputStr.toLocaleLowerCase().indexOf('fdi:')==0){
-             completeFDI(inputStr.toLocaleLowerCase(), e.target)
-
-         }
+         var inputStr = $(e.target).val().trim().toLocaleLowerCase()
+         if (inputStr.indexOf('fdi:')==0){
+             completeFDI(inputStr, e.target)
+         } else if (inputStr.indexOf('doi:')==0 || inputStr.indexOf('http://doi.org')==0 || inputStr.indexOf('https://doi.org')==0){
+            completeDOI(inputStr, e.target)
+        }
      }
  })
 var pendingHorizon;
@@ -45,10 +46,52 @@ var pendingHorizon;
             
         }
     }
+    function onError(){
+        alert("Ce numéro FDI est inconnu.\nUnkown FDI identifier.")
+    }
     pendingHorizon = $.ajax({
         type: "GET",
         url: url,
         dataType: "xml",
-        success: onXMLResponse(element, fdi)
+        success: onXMLResponse(element, fdi),
+        error: onError
+    })
+ }
+
+ var pendingDOI;
+ function completeDOI(doi, element) {
+    console.log("Autocomplete DOI")
+    doi = doi.replace('doi:','');
+    doi = doi.replace('http://doi.org/','');
+    doi = doi.replace('https://doi.org/','');
+    var url = "https://data.datacite.org/text/x-bibliography;style=harvard-cite-them-right/"+doi
+    if (pendingDOI != undefined){
+        pendingDOI.abort()
+    }
+    function onResponse(input_element,doi){
+        return function(data){
+            var elem = input_element
+            
+            var url = $(elem).parent().parent().parent().find('div:has(>label[for="metadata_publicationURL"])').find('input')
+            var citation = $(elem).parent().parent().parent().find('div:has(>label[for="metadata_publicationCitation"])').find('textarea')
+            var type = $(elem).parent().parent().parent().find('div:has(>label[for="metadata_publicationIDType"])').find('select')
+            var id = $(elem).parent().parent().parent().find('div:has(>label[for="metadata_publicationIDNumber"])').find('input')
+
+            
+            $(type).find('option').filter(function(){return $(this).text() == 'doi'}).first().prop('selected',true)
+            $(url).val("https://doi.org/"+doi)
+            $(citation).val(data)
+            $(id).val(doi)
+        }
+    }
+    function onError(){
+        alert("Ce numéro DOI est inconnu.\nUnkown DOI identifier.")
+    }
+    pendingDOI = $.ajax({
+        type: "GET",
+        url: url,
+        dataType: "text",
+        success: onResponse(element, doi),
+        error: onError
     })
  }
